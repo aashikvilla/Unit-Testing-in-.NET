046ea92e87513cccdcb2febba5856ad43d88877b
aashikvilla <aashikvilla99@gmail.com>
2021-09-30 18:39:31 +0530

    Add project files.

---
+++ app-test/CalculatorTest.cs
@@ -0,0 +1,50 @@
+using System;
+using app;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace app_test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [DataTestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void Add(int value)
+        {
+            Console.WriteLine("Testing {0} + {0}", value);
+            var actual = Calculator.Add(value, value);
+            var expected = value + value;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [DataTestMethod]
+        [DataRow(3,1)]
+        public void Subtract(int v1,int v2)
+        {
+          
+            Console.WriteLine("Testing {0} - {1}");
+            var actual = Calculator.Subtract(v1, v2);
+            var expected = v1 - v2;
+            Console.WriteLine("aaaaaaaaa", v1, v2);
+            Assert.AreEqual(actual, expected);
+
+        }
+
+        //[TestMethod]
+        //[TestCategory("DivideAndMultiply")]
+        //public void Divide()
+        //{
+        //    Assert.AreEqual(1, 1);
+        //}
+
+        //[TestMethod]
+        //[TestCategory("DivideAndMultiply")]
+        //public void Multiply()
+        //{
+        //    Assert.AreEqual(1, 1);
+        //}
+    }
+}
---
+++ app/Calculator.cs
@@ -0,0 +1,22 @@
+namespace app
+{
+    public class Calculator
+    {
+        public static int Add(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+        public static int Subtract(int lhs, int rhs)
+        {
+            return lhs - rhs;
+        }
+        public static int Multiply(int lhs, int rhs)
+        {
+            return lhs - rhs;
+        }
+        public static int Divide(int lhs, int rhs)
+        {
+            return lhs - rhs;
+        }
+    }
+}
